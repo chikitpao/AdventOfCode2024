cd49373ba57c56d5460963416291e6181c0889ba
Chi-Kit Pao <Chi-KitPao@gmx.de>
2024-12-13 22:44:38 +0100

    Added Solution for AoC 2024, Day 11, Part 2
    Also improved execution speed for part 1

--- Day11/Day11.jl
+++ Day11/Day11.jl
@@ -5,60 +5,72 @@
 """
 
 
+using DataStructures
+
 mutable struct Stone
-    text::String
     value::Int64
 end
 
 function parse_input(file_name::String)::Vector{Stone}
     stones = Vector{Stone}()
     for s ∈ split(readline(file_name))
-        push!(stones, Stone(s, parse(Int64, s)))
+        push!(stones, Stone(parse(Int64, s)))
     end
     return stones
 end
 
-function mutate!(stones::Vector{Stone})
-    i = 1
-    len = length(stones)
-    while i <= len
-        #println("mutate! i $i len $len")
-        stone = stones[i]
-        if stone.value == 0
-            stone.value = 1
-            stone.text = string(stone.value)
-        elseif length(stone.text) % 2 == 0
-            strlen = length(stone.text)
-            new_strlen = strlen ÷ 2
-            new_stone = Stone("", parse(Int64, stone.text[(new_strlen+1):strlen]))
-            new_stone.text = string(new_stone.value)
-            stone.value = parse(Int64, stone.text[1:new_strlen])
-            stone.text = string(stone.value)
-            insert!(stones, i + 1, new_stone)
-            len += 1
-            i += 1
-        else
-            stone.value *= 2024
-            stone.text = string(stone.value)
+function answer(stones::Vector{Stone}, rounds::Int64)::Int64
+    dict = Dict{Int64,Vector{Int64}}()
+    ddict = DefaultDict{Int64, Int64}(0)
+    
+    for stone ∈ stones
+        ddict[stone.value] += 1
+    end
+    
+    new_ddict = DefaultDict{Int64, Int64}(0)
+    for i ∈ 1:rounds
+        for (k, v) ∈ pairs(ddict)
+            if k == 0
+                new_ddict[1] += v
+            else
+                mutation = []
+                if haskey(dict, k)
+                    mutation = dict[k]
+                else
+                    s = string(k)
+                    strlen = length(s)
+                    if strlen % 2 == 0
+                        new_strlen = strlen ÷ 2
+                        mutation = [parse(Int64, s[1:new_strlen]), parse(Int64, s[(new_strlen+1):strlen])]
+                    else
+                        mutation = [k * 2024]
+                    end
+                    dict[k] = mutation
+                end
+                for m ∈ mutation
+                    new_ddict[m] += v
+                end
+            end
         end
-        i += 1
+        ddict = new_ddict
+        new_ddict = DefaultDict{Int64, Int64}(0)
     end
-end
-
-function part1(stones::Vector{Stone})::Int64
-    for i ∈ 1:25
-        mutate!(stones)
-    end
-    return length(stones)
+    
+    return sum(collect(values(ddict)))
 end
 
 function main()
+    stones = parse_input("input.txt")
     println("Question 1: How many stones will you have after blinking 25 times?")
-    println("Answer: $(part1(parse_input("input.txt")))")
+    println("Answer: $(answer(stones, 25))")
+    println("Question 2: How many stones would you have after blinking a total of 75 times?")
+    println("Answer: $(answer(stones, 75))")
 end
 
 @time main()
 
 # Question 1: How many stones will you have after blinking 25 times?
 # Answer: 186175
-#  1.598008 seconds (1.65 M allocations: 47.751 MiB, 2.91% gc time, 0.34% compilation time)
+# Question 2: How many stones would you have after blinking a total of 75 times?
+# Answer: 220566831337810
+#  0.023043 seconds (36.63 k allocations: 14.445 MiB, 23.87% compilation time)
