9ea6a76f89dfffd2cf22b71b834cecf6cf54431f
Chi-Kit Pao <Chi-KitPao@gmx.de>
2024-12-22 13:46:28 +0100

    Added Solution for AoC 2024, Day 19, Part 2

--- Day19/Day19.jl
+++ Day19/Day19.jl
@@ -19,7 +19,7 @@ function parse_input(file_name::String)::Vector{String}
     return lines[3:length(lines)]
 end
 
-@memoize function is_valid(design::String)
+@memoize function is_valid(design::String)::Bool
     for p ∈ patterns
         if !startswith(design, p.pattern)
             continue
@@ -41,15 +41,41 @@ function part1(designs::Vector{String})::Int64
     return count(results)
 end
 
+@memoize function check_variations(design::String)::Int64
+    result = 0
+    for p ∈ patterns
+        if !startswith(design, p.pattern)
+            continue
+        end
+        if length(design) == p.length_
+            result += 1
+        else
+            result += check_variations(design[(p.length_+1):end])
+        end
+    end
+    return result
+end
+
+function part2(designs::Vector{String})::Int64
+    results = [check_variations(d) for d ∈ designs]
+    return sum(results)
+end
+
 function main()
-    println("Question 1: How many designs are possible?")
     designs = parse_input("input.txt")
-    answer1 = part1(designs)
-    println("Answer: $answer1")
+    
+    println("Question 1: How many designs are possible?")
+    println("Answer: $(part1(designs))")
+
+    println("Question 2: What do you get if you add up the number of different ways you could make each design?")
+    println("Answer: $(part2(designs))")
 end
 
 @time main()
 
 # Question 1: How many designs are possible?
 # Answer: 324
-#  0.717287 seconds (11.82 M allocations: 362.343 MiB, 6.14% gc time, 8.18% compilation time)
+# Question 2: What do you get if you add up the number of different ways you could make each design?
+# Answer: 575227823167869
+#  2.191498 seconds (37.11 M allocations: 1.108 GiB, 6.32% gc time, 4.29% compilation time)
+
